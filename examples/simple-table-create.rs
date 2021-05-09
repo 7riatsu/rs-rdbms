0be191f04e86ccbf2ad22cf7192452ca8a9f3aed
7riatsu <atsuki02jojo@gmail.com>
2021-05-09 19:17:19 +0900

    Change to use Table instead of SimpleTable

--- examples/simple-table-create.rs
+++ examples/simple-table-create.rs
@@ -2,17 +2,28 @@ use anyhow::Result;
 
 use rs_rdbms::buffer::{BufferPool, BufferPoolManager};
 use rs_rdbms::disk::{DiskManager, PageId};
-use rs_rdbms::table::SimpleTable;
+use rs_rdbms::table::{Table, UniqueIndex};
 
 fn main() -> Result<()> {
-    let disk = DiskManager::open("simple.rly")?;
+    // let disk = DiskManager::open("simple.rly")?;
+    let disk = DiskManager::open("table.rly")?;
     let pool = BufferPool::new(10);
     let mut bufmgr = BufferPoolManager::new(disk, pool);
 
-    let mut table = SimpleTable {
-        meta_page_id: PageId(0),
+    // let mut table = SimpleTable {
+    //     meta_page_id: PageId(0),
+    //     num_key_elems: 1,
+    // };
+
+    let mut table = Table {
+        meta_page_id: PageId::INVALID_PAGE_ID,
         num_key_elems: 1,
+        unique_indices: vec![UniqueIndex {
+            meta_page_id: PageId::INVALID_PAGE_ID,
+            skey: vec![2],
+        }],
     };
+
     table.create(&mut bufmgr)?;
     dbg!(&table);
     table.insert(&mut bufmgr, &[b"z", b"Alice", b"Smith"])?;
@@ -20,7 +31,6 @@ fn main() -> Result<()> {
     table.insert(&mut bufmgr, &[b"y", b"Charlie", b"Williams"])?;
     table.insert(&mut bufmgr, &[b"w", b"Dave", b"Miller"])?;
     table.insert(&mut bufmgr, &[b"v", b"Eve", b"Brown"])?;
-    
     bufmgr.flush()?;
     Ok(())
 }
